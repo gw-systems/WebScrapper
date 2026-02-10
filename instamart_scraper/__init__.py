4ef06245ad97d7fad2a89a07ff2fc0af99d6997f
krishi-DA <krishi@godamwale.com>
2026-02-10 12:17:03 +0530

    Cleanup: Final root directory cleanup, removing temporary, debug, and legacy files

--- instamart_scraper/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Instamart API Scraper Module
-"""
-
-from .api_scraper import InstamartAPIScraper
-from .product_parser import parse_products_from_response, parse_product
-
-__all__ = ['InstamartAPIScraper', 'parse_products_from_response', 'parse_product']
